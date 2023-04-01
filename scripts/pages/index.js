9e3c7f71f4cd87a7eecd74ef77733609144eddf3
farah <farah.manoubi@hotmail.com>
2023-04-01 13:23:20 +0200

    fetch done

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,47 +1,16 @@
     async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-       /* let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]}) */
-
-
             fetch("data/photographers.json")
                 .then(result => {
-                    console.log(result)
+                    //console.log(result);
                     return result.json();
                 })
                 .then(data => {
-                    console.log(data.photographers);
-                    displayData(data);
+                   //console.log(data.photographers);
+                    return data;
                 })
                 .catch(e => {
                     console.log(e);
                 });
-           /* .then(photographersFile => photographersFile.json())
-            .then(data =>displayData(data)); */
-
-         
     }
   
 
@@ -50,11 +19,8 @@
         const photographersSection = document.querySelector(".photographer_section");
         const photographe = await fetch("data/photographers.json");
         const displayPhotograph = await photographe.json();
-        
-        console.log(displayPhotograph);
-        console.log(Object.keys(displayPhotograph.photographers));
 
-        Object.keys(displayPhotograph.photographers).forEach((photographer) => {
+        Array.from(displayPhotograph.photographers).forEach((photographer) => {
             const photographerModel = photographerFactory(photographer);
             const userCardDOM = photographerModel.getUserCardDOM();
             photographersSection.appendChild(userCardDOM);
@@ -65,10 +31,8 @@
 
     async function init() {
         // Récupère les datas des photographes
-       // const { photographers } = await getPhotographers();
-       // getPhotographers().then(photographersFile => console.log(photographersFile))
-        await getPhotographers();
-        //displayData(photographers);
+        const photographers = await getPhotographers();
+        displayData(photographers);
     };
     
     init();
