8cc92af0614b2143e6af23f6b9b9a0ca1c75c129
farah <farah.manoubi@hotmail.com>
2023-04-05 17:51:34 +0200

    css

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,5 +1,5 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -8,10 +8,21 @@ function photographerFactory(data) {
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
+        const h3City = document.createElement( 'h3' );
+        const h3Country = `, ${country}`;
+        const h4 = document.createElement( 'h4' );
+        const p = document.createElement('p');
         h2.textContent = name;
+        h3City.textContent = city;
+        h4.textContent = `${price}€/jour`;
+        p.textContent = tagline;
+        h3City.insertAdjacentHTML('beforeend', h3Country);
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3City);
+        article.appendChild(p);
+        article.appendChild(h4);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, city, country, tagline, price, picture, getUserCardDOM }
 }
