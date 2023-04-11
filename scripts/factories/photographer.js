76cace34fc4872dd4c0d27a012d64c3b887bb2b6
farah <farah.manoubi@hotmail.com>
2023-04-12 00:29:21 +0200

    cards photographers cliquable

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,15 +1,21 @@
 function photographerFactory(data) {
-    const { name, city, country, tagline, price, portrait } = data;
+    const { name, id, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
-
+    const url = `photographer.html?id=${id}`;
+    
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
+        const lien = document.createElement( 'a' );
+        const contain = document.createElement( 'div' );
         
         img.setAttribute("src", picture);
         img.setAttribute("alt", name);
 
+        lien.setAttribute("href", url);
+        lien.setAttribute("aria-label", name);
+    
         const h2 = document.createElement( 'h2' );
         const h3City = document.createElement( 'h3' );
         const h3Country = `, ${country}`;
@@ -22,13 +28,16 @@ function photographerFactory(data) {
         p.textContent = tagline;
 
         h3City.insertAdjacentHTML('beforeend', h3Country);
-        article.appendChild(img);
-        article.appendChild(h2);
+        article.appendChild(contain);
+        contain.appendChild(img);
+        contain.appendChild(h2);
+        contain.appendChild(lien);
         article.appendChild(h3City);
         article.appendChild(p);
         article.appendChild(h4);
-
+        
+        
         return (article);
     }
-    return { name, city, country, tagline, price, picture, getUserCardDOM }
+    return { name, id, city, country, tagline, price, picture, getUserCardDOM }
 }
