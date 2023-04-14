6978c0d51c334a11e1d597a52b1d2cbc3bf49b5d
farah <farah.manoubi@hotmail.com>
2023-04-14 23:36:14 +0200

    no change

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -2,16 +2,11 @@
     async function getPhotograph() {  
         const id = new URL(location.href).searchParams.get("id");
         console.log(id)
+
         const result = await fetch(`photographer.html?id=${id}`);
+        console.log(result)
 
         return await result.json();
     }
 
-    async function getPhotographProfil() {
-        
-        const photograph = await getPhotograph();
-        console.log(photograph);
-        
-    }
-
-    getPhotographProfil();
+    getPhotograph();
